84f866b8dc5a0ba22eee60822196802ab16e4433
kashif0426 <sayedkaskai22@gmail.com>
2022-01-19 20:49:04 +0100

    i am working on JS

---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+
+let name='Mosh';
+console.log(name)
+
+// Cannot be a reserved keyword
+//Should be meaningful
+//Cannot start whith a number (1name)
+//Cannot contain a space or hyphen(-)
+//Are case-senssitive
+let firstName = 'Mosh'; lastName = 'Hamedani'
